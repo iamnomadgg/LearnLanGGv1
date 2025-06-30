625019d748460694bac06a559dbd30e6869ee70c
Gokhan Gocmen <gokhan.gocmen@dxc.com>
2025-06-30 11:54:53 +0200

    feat: use react-route-dom to navigate the user to add a lesson

--- client/learn-langg-client/src/App.jsx
+++ client/learn-langg-client/src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import LessonList from './components/LessonList';
+import LessonForm from './components/LessonForm';
+import { Button, Container, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <h1>LearnLangg</h1>
-      <LessonList />
-    </div>
+    <Router>
+      <Container maxWidth="md" sx={{ mt: 4 }}>
+        <Typography variant="h4" gutterBottom>
+          LearnLangg
+        </Typography>
+
+        <Button variant="contained" component={Link} to="/add" sx={{ mb: 3 }}>
+          Add New Lesson
+        </Button>
+
+        <Routes>
+          <Route path="/" element={<LessonList />} />
+          <Route path="/add" element={<LessonForm />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
